f2a55b2528a04f9b1368b88a37362cdd9bed76a8
Ana Durand <ana.dc1004@gmail.com>
2017-06-06 10:01:34 -0500

    arreglando funcion header

--- js/components/header.js
+++ js/components/header.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Header = (update) => {
-  const header = $("<header></header");
+  const header = $("<header></header>");
   const container = $('<div class="container"></div>');
   const h1 = $("<h1>To Do App</h1>");
 
